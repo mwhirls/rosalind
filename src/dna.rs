a20cdbe89dbb73d8be31d934047bea55e2fbb4a7
mwhirls <megan.worley@live.com>
2024-02-06 12:10:20 -0800

    Solution to first problem

--- src/dna.rs
+++ src/dna.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let sequence = &args[1];
+    let result: Vec<usize> = "ACGT"
+        .chars()
+        .into_iter()
+        .map(|base| sequence.chars().filter(|c| *c == base).count())
+        .collect();
+    println!("{} {} {} {}", result[0], result[1], result[2], result[3]);
 }
